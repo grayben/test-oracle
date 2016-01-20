847494c3e68655ffe7da8e3d4fa77b0accda5587
Ben Gray <gray.ben.m@gmail.com>
2016-01-20 16:22:43 +1100

    Add parametric test oracle

---
+++ src/main/java/com/grayben/tools/testOracle/ParametricTestOracle.java
@@ -0,0 +1,37 @@
+package com.grayben.tools.testOracle;
+
+import com.grayben.tools.math.parametricEquation.ParametricEquation;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by beng on 20/01/2016.
+ */
+public class ParametricTestOracle<P, I, O> {
+    private final ParametricEquation<P, I, O> parametricEquation;
+
+    public ParametricTestOracle(ParametricEquation<P, I, O> parametricEquation) {
+        this.parametricEquation = parametricEquation;
+    }
+
+    public Pair<I, O> getInputAndExpectedOutput(P parameter) {
+        return parametricEquation.apply(parameter);
+    }
+
+    public void validateOutputEqualsExpected(Function<I, O> operation, P parameter){
+        Pair<I, O> oraclePair = parametricEquation.apply(parameter);
+        I input = oraclePair.getLeft();
+        O expectedOutput = oraclePair.getRight();
+        O actualOut = operation.apply(input);
+
+        String notEqualMessage = "Based on the parameter " + parameter.toString() + ", this oracle " +
+                "generated the input variable " + input.toString() + ".\n" +
+                "This oracle determined that the expected output is " + expectedOutput.toString() + ". " +
+                "However, the actual function output was " + actualOut.toString() + ".";
+
+        assertEquals(notEqualMessage, expectedOutput, actualOut);
+    }
+}
