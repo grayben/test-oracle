47479fee82c2796431f2d9c59ba059730585f306
Ben Gray <gray.ben.m@gmail.com>
2016-01-20 15:17:57 +1100

    Implement InputOutputPair using org.apache.commons.lang3.tuple.Pair backing

--- src/main/java/com/grayben/tools/testOracle/InputOutputPair.java
+++ src/main/java/com/grayben/tools/testOracle/InputOutputPair.java
@@ -1,15 +1,24 @@
 package com.grayben.tools.testOracle;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 /**
  * Created by beng on 20/01/2016.
  */
 public class InputOutputPair<I, O> implements InputOutputPairable<I, O> {
 
+    private final Pair<I, O> pair;
+
+    public InputOutputPair(I input, O output) {
+        this.pair = new ImmutablePair<I, O>(input, output);
+    }
+
     public I input() {
-        return null;
+        return pair.getLeft();
     }
 
     public O output() {
-        return null;
+        return pair.getRight();
     }
 }
