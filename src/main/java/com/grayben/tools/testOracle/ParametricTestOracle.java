e696c0eaece75324d1004898862d7132deb3250d
Ben Gray <gray.ben.m@gmail.com>
2016-01-24 20:12:50 +1100

    Extract TestOracle interface, create DirectTestOracle (extends ParametricTestOracle

--- src/main/java/com/grayben/tools/testOracle/ParametricTestOracle.java
+++ src/main/java/com/grayben/tools/testOracle/ParametricTestOracle.java
@@ -6,19 +6,19 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by beng on 20/01/2016.
  */
-public class ParametricTestOracle<P, I, O> {
+public class ParametricTestOracle<P, I, O> implements TestOracle<P, I, O> {
     private final ParametricEquation<P, I, O> parametricEquation;
 
     public ParametricTestOracle(ParametricEquation<P, I, O> parametricEquation) {
         this.parametricEquation = parametricEquation;
     }
 
-    public void validateOutputEqualsExpected(Function<I, O> operation, P parameter){
+    @Override
+    public void validate(Function<I, O> operation, P parameter){
         Pair<I, O> oraclePair = getInputAndExpectedOutput(parameter);
         I input = oraclePair.getLeft();
         O expectedOutput = oraclePair.getRight();
@@ -32,21 +32,12 @@ public class ParametricTestOracle<P, I, O> {
         assertEquals(notEqualMessage, expectedOutput, actualOut);
     }
 
-    public void validateOutputNotEqualsExpected(Function<I, O> operation, P parameter){
-        Pair<I, O> oraclePair = getInputAndExpectedOutput(parameter);
-        I input = oraclePair.getLeft();
-        O expectedOutput = oraclePair.getRight();
-        O actualOut = operation.apply(input);
-
-        String equalMessage = "Based on the parameter " + parameter.toString() + ", this oracle " +
-                "generated the input variable " + input.toString() + ".\n" +
-                "This oracle determined that the expected output is " + expectedOutput.toString() + ". " +
-                "However, the actual function output was also " + actualOut.toString() + ".";
-
-        assertNotEquals(equalMessage, expectedOutput, actualOut);
-    }
-
     private Pair<I, O> getInputAndExpectedOutput(P parameter) {
+        if (parameter == null) {
+            throw new NullPointerException(
+                    "Argument 'parameter' is null"
+            );
+        }
         return parametricEquation.apply(parameter);
     }
 }
