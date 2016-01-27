b8f476b33a2138acbc1522a49ed2d55fc42db4e5
Ben Gray <gray.ben.m@gmail.com>
2016-01-27 14:00:28 +1100

    Revert "Change interface"
    This reverts commit 72e1bd2a6bc7df9afa19e874c24fefb2eb3d8655.

--- src/main/java/com/grayben/tools/testOracle/TestOracle.java
+++ src/main/java/com/grayben/tools/testOracle/TestOracle.java
@@ -1,19 +1,10 @@
 package com.grayben.tools.testOracle;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import java.util.function.Function;
 
 /**
  * Created by beng on 24/01/2016.
  */
-public interface TestOracle<SUT, P, I, O> {
-    class F1<SUT, I, O> implements Function<Pair<SUT, I>, O> {
-
-        @Override
-        public O apply(Pair<SUT, I> sutInputPair) {
-            return null;
-        }
-    }
-    void validate(SUT systemUnderTest, Function<I, O> operation, P parameter);
+public interface TestOracle<P, I, O> {
+    void validate(Function<I, O> operation, P parameter);
 }
