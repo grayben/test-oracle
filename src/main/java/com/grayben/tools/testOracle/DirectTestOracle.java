691ce80e9cb93b6f0c73c4b49e353c52bf0277b8
Ben Gray <gray.ben.m@gmail.com>
2016-01-27 14:27:59 +1100

    All concrete: directly implement TestOracle, use compositional reuse not inherit

--- src/main/java/com/grayben/tools/testOracle/DirectTestOracle.java
+++ src/main/java/com/grayben/tools/testOracle/DirectTestOracle.java
@@ -7,12 +7,19 @@ import java.util.function.Function;
 /**
  * Created by beng on 24/01/2016.
  */
-public class DirectTestOracle<I, O> extends ParametricTestOracle<I, I, O>{
+public class DirectTestOracle<I, O> implements TestOracle<I, I, O> {
+
+    private final ParametricTestOracle<I, I, O> parametricTestOracle;
 
     public DirectTestOracle(Function<I, O> directEquation) {
-        super(i -> {
+        this.parametricTestOracle = new ParametricTestOracle<>(i -> {
             Function<I, I> identity = Function.identity();
             return new ImmutablePair<>(identity.apply(i), directEquation.apply(i));
         });
+    }
+
+    @Override
+    public void validate(Function<I, O> operation, I parameter) {
+        this.parametricTestOracle.validate(operation, parameter);
     }
 }
