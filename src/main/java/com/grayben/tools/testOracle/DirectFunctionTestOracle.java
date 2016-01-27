48bc8f47d8c5a1093b7a18b9a8f6a2901ac65604
Ben Gray <gray.ben.m@gmail.com>
2016-01-27 14:44:25 +1100

    Rename to make behaviour more apparent

--- src/main/java/com/grayben/tools/testOracle/DirectFunctionTestOracle.java
+++ src/main/java/com/grayben/tools/testOracle/DirectFunctionTestOracle.java
@@ -7,11 +7,11 @@ import java.util.function.Function;
 /**
  * Created by beng on 24/01/2016.
  */
-public class DirectTestOracle<I, O> implements TestOracle<I, I, O> {
+public class DirectFunctionTestOracle<I, O> implements TestOracle<I, I, O> {
 
     private final ParametricTestOracle<I, I, O> parametricTestOracle;
 
-    public DirectTestOracle(Function<I, O> directEquation) {
+    public DirectFunctionTestOracle(Function<I, O> directEquation) {
         this.parametricTestOracle = new ParametricTestOracle<>(i -> {
             Function<I, I> identity = Function.identity();
             return new ImmutablePair<>(identity.apply(i), directEquation.apply(i));
