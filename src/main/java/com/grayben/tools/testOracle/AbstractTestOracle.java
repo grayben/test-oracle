245b18737a7fa61b1baa4636e87319cc0757b5f8
Ben Gray <gray.ben.m@gmail.com>
2016-01-28 12:19:58 +1100

    Add AbstractTestOracle

---
+++ src/main/java/com/grayben/tools/testOracle/AbstractTestOracle.java
@@ -0,0 +1,38 @@
+package com.grayben.tools.testOracle;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Created by beng on 28/01/2016.
+ */
+public abstract class AbstractTestOracle<I, O>{
+
+
+    private Supplier<I> inputSupplier;
+
+    public abstract class VerificationProvider<I, O> {
+
+        public abstract boolean verify(I input, O output);
+
+        public abstract class AlternateImplementation<I, O> implements Function<I, O>{}
+    }
+
+
+    public AbstractTestOracle(VerificationProvider<I, O> verificationProvider) {
+        this.systemUnderTest = systemUnderTest();
+        this.verificationProvider = verificationProvider;
+    }
+
+    protected abstract Function<I,O> systemUnderTest();
+
+    private final Function<I, O> systemUnderTest;
+
+    public boolean validate() {
+        I input = inputSupplier.get();
+        O actualOutput = systemUnderTest.apply(input);
+        return verificationProvider.verify(input, actualOutput);
+    }
+
+    private final VerificationProvider<I, O> verificationProvider;
+}
