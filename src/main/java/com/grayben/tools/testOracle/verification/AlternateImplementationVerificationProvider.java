9e116c39dd1eb51c38ab228fcf101e2b69768d17
Ben Gray <gray.ben.m@gmail.com>
2016-01-28 13:18:20 +1100

    Add AlternateImplementationVerificationProvicer

---
+++ src/main/java/com/grayben/tools/testOracle/verification/AlternateImplementationVerificationProvider.java
@@ -0,0 +1,20 @@
+package com.grayben.tools.testOracle.verification;
+
+import java.util.function.Function;
+
+/**
+ * Created by beng on 28/01/2016.
+ */
+public class AlternateImplementationVerificationProvider<I, O> implements VerificationProvider<I, O> {
+
+    private final Function<I, O> alternateImplementation;
+
+    public AlternateImplementationVerificationProvider(Function<I, O> alternateImplementation) {
+        this.alternateImplementation = alternateImplementation;
+    }
+
+    @Override
+    public boolean test(I input, O output) {
+        return output == alternateImplementation.apply(input);
+    }
+}
