b213acc759747425ca7ec74728fee0d7add487a7
Ben Gray <gray.ben.m@gmail.com>
2016-01-28 11:59:21 +1100

    Remove parameter from interface

--- src/main/java/com/grayben/tools/testOracle/TestOracle.java
+++ src/main/java/com/grayben/tools/testOracle/TestOracle.java
@@ -5,6 +5,6 @@ import java.util.function.Function;
 /**
  * Created by beng on 24/01/2016.
  */
-public interface TestOracle<P, I, O> {
-    void validate(Function<I, O> operation, P parameter);
+public interface TestOracle<I, O> {
+    boolean validate(Function<I, O> operation, I input);
 }
