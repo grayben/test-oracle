d5c5eb93ab7d56e3edb6cb1cf90a13ca2ca6b03d
Ben Gray <gray.ben.m@gmail.com>
2016-01-28 13:07:14 +1100

    Make Verification simply rename the BiPredicate interface

--- src/main/java/com/grayben/tools/testOracle/verification/VerificationProvider.java
+++ src/main/java/com/grayben/tools/testOracle/verification/VerificationProvider.java
@@ -1,10 +1,9 @@
 package com.grayben.tools.testOracle.verification;
 
+import java.util.function.BiPredicate;
+
 /**
  * Created by beng on 28/01/2016.
  */
-public interface VerificationProvider<I, O> {
-
-    boolean verify(I input, O output);
-
+public interface VerificationProvider<I, O> extends BiPredicate<I, O> {
 }
