e37eb5a49140411eac7f10954a6b6372135403ac
Ben Gray <gray.ben.m@gmail.com>
2016-01-28 16:46:02 +1100

    Add EnumInputAdapter

---
+++ src/main/java/com/grayben/tools/testOracle/oracle/input/EnumInputAdapter.java
@@ -0,0 +1,22 @@
+package com.grayben.tools.testOracle.oracle.input;
+
+import java.util.EnumMap;
+
+/**
+ * Created by beng on 28/01/2016.
+ */
+public abstract class EnumInputAdapter<E extends Enum<E>, I> extends InputAdapter<E, I> {
+
+    private final EnumMap<E, I> enumMap;
+
+    public EnumInputAdapter() {
+        enumMap = enumMap();
+    }
+
+    protected abstract EnumMap<E, I> enumMap();
+
+    @Override
+    public I apply(E e) {
+        return enumMap.get(e);
+    }
+}
