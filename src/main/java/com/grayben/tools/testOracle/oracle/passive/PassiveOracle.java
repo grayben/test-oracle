41da00ba3215e11ee8ac69724959fb2123f27bdf
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 08:20:35 +1100

    Override BiPredicate parameter names for clarity

--- src/main/java/com/grayben/tools/testOracle/oracle/passive/PassiveOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/passive/PassiveOracle.java
@@ -6,4 +6,7 @@ import java.util.function.BiPredicate;
  * Created by beng on 28/01/2016.
  */
 public interface PassiveOracle<I, O> extends BiPredicate<I, O> {
+
+    @Override
+    boolean test(I input, O actualOutput);
 }
