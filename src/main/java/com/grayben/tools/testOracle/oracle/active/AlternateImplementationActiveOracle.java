fa319c3ec7d6d726fb4a1d11d83c25fa989e8791
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 09:36:17 +1100

    Make AlternateImplementation...Oracle active, modify tests

---
+++ src/main/java/com/grayben/tools/testOracle/oracle/active/AlternateImplementationActiveOracle.java
@@ -0,0 +1,20 @@
+package com.grayben.tools.testOracle.oracle.active;
+
+import java.util.function.Function;
+
+/**
+ * Created by beng on 28/01/2016.
+ */
+final public class AlternateImplementationActiveOracle<I, O> implements ActiveOracle<I, O> {
+
+    private final Function<I, O> alternateImplementation;
+
+    public AlternateImplementationActiveOracle(Function<I, O> alternateImplementation) {
+        this.alternateImplementation = alternateImplementation;
+    }
+
+    @Override
+    public O apply(I input) {
+        return alternateImplementation.apply(input);
+    }
+}
