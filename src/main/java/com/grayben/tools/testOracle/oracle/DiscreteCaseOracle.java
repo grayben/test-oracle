e98c44004f7b61fbdb9ae0240bc20c18e1354437
Ben Gray <gray.ben.m@gmail.com>
2016-01-29 18:07:21 +1100

    Change Options to Option and add some rough scribbling

--- src/main/java/com/grayben/tools/testOracle/oracle/DiscreteCaseOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/DiscreteCaseOracle.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import com.grayben.tools.testOracle.oracle.input.EnumAdapter;
 import com.grayben.tools.testOracle.verification.DiscreteCaseVerificationProvider;
 import com.grayben.tools.testOracle.verification.VerificationProvider;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.EnumMap;
 import java.util.HashMap;
@@ -47,9 +49,9 @@ public abstract class DiscreteCaseOracle<E extends Enum<E>, I, O> extends Oracle
     protected abstract DiscreteCaseVerificationProvider<E, O> discreteCaseVerificationProvider();
 
     public static class ConcreteDiscreteCaseOracle
-            extends DiscreteCaseOracle<ConcreteDiscreteCaseOracle.Options, Integer, String>{
+            extends DiscreteCaseOracle<ConcreteDiscreteCaseOracle.Option, Integer, String>{
         @Override
-        protected Function<Options, Function<Integer, String>> underlyingSystemUnderTestFunction() {
+        protected Function<Option, Function<Integer, String>> underlyingSystemUnderTestFunction() {
             return options -> {
                 switch (options){
                     case SIMPLE:
@@ -62,35 +64,53 @@ public abstract class DiscreteCaseOracle<E extends Enum<E>, I, O> extends Oracle
         //TODO: fix low cohesion: enum adapter and verification provider both declaring different sides of a relation
 
         @Override
-        protected EnumAdapter<Options, Integer> enumAdapter() {
-            return new EnumAdapter<Options, Integer>() {
+        protected EnumAdapter<Option, Integer> enumAdapter() {
+            return new EnumAdapter<Option, Integer>() {
                 @Override
-                protected EnumMap<Options, Integer> enumMap() {
-                    Map<Options, Integer> map = new HashMap<>();
+                protected EnumMap<Option, Integer> enumMap() {
+                    Map<Option, Integer> map = new HashMap<>();
                     // TODO: fix low cohesion: enum adapter and verification provider
                     // TODO: both declaring different sides of a relation
-                    map.put(Options.SIMPLE, 1);
+                    map.put(Option.SIMPLE, 1);
                     return new EnumMap<>(map);
                 }
             };
         }
 
         @Override
-        protected DiscreteCaseVerificationProvider<Options, String> discreteCaseVerificationProvider(){
-            return new DiscreteCaseVerificationProvider<Options, String>() {
+        protected DiscreteCaseVerificationProvider<Option, String> discreteCaseVerificationProvider(){
+            return new DiscreteCaseVerificationProvider<Option, String>() {
                 @Override
-                protected Map<Options, String> casePairs() {
-                    Map<Options, String> map = new HashMap<>();
+                protected Map<Option, String> casePairs() {
+                    Map<Option, String> map = new HashMap<>();
                     Integer theRightSimpleInput = 1;
                     //TODO: fix low cohesion: enum adapter and verification provider
                     //TODO: both declaring different sides of a relation
-                    map.put(Options.SIMPLE, String.valueOf(2 * theRightSimpleInput));
+                    map.put(Option.SIMPLE, String.valueOf(2 * theRightSimpleInput));
                     return ImmutableMap.copyOf(map);
                 }
             };
         }
 
-        public enum Options {
+        private Function<Option, Pair<Integer, String>> pairGenerator(){
+            return new Function<Option, Pair<Integer, String>>() {
+                @Override
+                public Pair<Integer, String> apply(Option option) {
+                    return null;
+                }
+            };
+        }
+
+        protected Pair<Integer, String> getPair(Option option){
+
+            switch (option) {
+                case SIMPLE:
+                    return new ImmutablePair<>(1, "One");
+            }
+            throw new IllegalArgumentException("The option was not recognised");
+        }
+
+        public enum Option {
             SIMPLE
         }
 
