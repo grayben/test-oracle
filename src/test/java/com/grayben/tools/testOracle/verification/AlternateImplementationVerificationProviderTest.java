44c874106a995019945d8891b84ec0e62da74dcc
Ben Gray <gray.ben.m@gmail.com>
2016-01-29 12:29:27 +1100

    Update tests

--- src/test/java/com/grayben/tools/testOracle/verification/AlternateImplementationVerificationProviderTest.java
+++ src/test/java/com/grayben/tools/testOracle/verification/AlternateImplementationVerificationProviderTest.java
@@ -7,6 +7,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.function.Function;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -15,21 +16,34 @@ import static org.junit.Assert.assertTrue;
 @RunWith(MockitoJUnitRunner.class)
 public class AlternateImplementationVerificationProviderTest {
 
-    private Function<Integer, String> theFunction;
+    private Function<Integer, String> thePrimaryImplementation;
+    private Function<Integer, String> theAlternateImplementation;
     private AlternateImplementationVerificationProvider<Integer, String> alternateImplementationVerificationProvider;
 
     @After
     public void tearDown() throws Exception {
-        theFunction = null;
+        thePrimaryImplementation = null;
+        theAlternateImplementation = null;
         alternateImplementationVerificationProvider = null;
     }
 
     @Test
     public void test_TestReturnsTrue_WhenFunctionUnderTestAndAlternateImplementationAreTheSame() throws Exception {
-        theFunction = integer -> String.valueOf(2 * integer);
-        alternateImplementationVerificationProvider = new AlternateImplementationVerificationProvider<>(theFunction);
+        thePrimaryImplementation = integer -> String.valueOf(2 * integer);
+        theAlternateImplementation = thePrimaryImplementation;
+        alternateImplementationVerificationProvider = new AlternateImplementationVerificationProvider<>(theAlternateImplementation);
         Integer input = 4456;
-        String actualOutput = theFunction.apply(input);
+        String actualOutput = thePrimaryImplementation.apply(input);
         assertTrue(alternateImplementationVerificationProvider.test(input, actualOutput));
+    }
+
+    @Test
+    public void test_TestReturnsFalse_WhenFunctionUnderTestAndAlternateImplementationAreNotTheSame() throws Exception {
+        theAlternateImplementation = integer -> String.valueOf(2 * integer);
+        thePrimaryImplementation = integer -> theAlternateImplementation.apply(integer).concat(" and then some more");
+        alternateImplementationVerificationProvider = new AlternateImplementationVerificationProvider<>(theAlternateImplementation);
+        Integer input = 4456;
+        String actualOutput = thePrimaryImplementation.apply(input);
+        assertFalse(alternateImplementationVerificationProvider.test(input, actualOutput));
     }
 }
