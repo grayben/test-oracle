aa8d368f3053cd72b6198cb68a23838a6cb6b93a
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 11:46:33 +1100

    Comment ActiveOracle

--- src/main/java/com/grayben/tools/testOracle/oracle/active/ActiveOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/active/ActiveOracle.java
@@ -5,12 +5,26 @@ import com.grayben.tools.testOracle.oracle.passive.PassiveOracle;
 import java.util.function.Function;
 
 /**
- * Created by beng on 30/01/2016.
+ * A simple facade for {@link Function} which defines an active test oracle.
+ * <p/>
+ * Created by Ben Gray on 30/01/2016.
+ * @param <I> the input type
+ * @param <O> the output type
  */
 public interface ActiveOracle<I, O> extends Function<I, O> {
 
+    /**
+     * Determine the expected output on a given input.
+     * <p>
+     * @param input the input
+     * @return the expected output for {@code input}
+     */
     @Override
     O apply(I input);
 
+    /**
+     * Convert this active oracle to a passive oracle.
+     * @return the adapted oracle
+     */
     PassiveOracle<I, O> adaptToPassiveOracle();
 }
