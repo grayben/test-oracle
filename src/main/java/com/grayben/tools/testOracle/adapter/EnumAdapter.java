552e8e9eebd63eca1126edf3046f21b24fedb26d
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 11:23:10 +1100

    Document EnumAdapter

--- src/main/java/com/grayben/tools/testOracle/adapter/EnumAdapter.java
+++ src/main/java/com/grayben/tools/testOracle/adapter/EnumAdapter.java
@@ -3,7 +3,11 @@ package com.grayben.tools.testOracle.adapter;
 import java.util.EnumMap;
 
 /**
- * Created by beng on 28/01/2016.
+ * An implementation of {@link Adapter} leveraging the efficiency of an {@link EnumMap}
+ * <p>
+ * Created by Ben Gray on 28/01/2016.
+ * @param <E> input enum type
+ * @param <Y> output type
  */
 final public class EnumAdapter<E extends Enum<E>, Y> implements Adapter<E, Y> {
 
@@ -13,8 +17,12 @@ final public class EnumAdapter<E extends Enum<E>, Y> implements Adapter<E, Y> {
         this.enumMap = enumMap;
     }
 
+    /**
+     * @param input the input to adapt
+     * @return the adapted input
+     */
     @Override
-    public Y apply(E e) {
-        return enumMap.get(e);
+    public Y apply(E input) {
+        return enumMap.get(input);
     }
 }
