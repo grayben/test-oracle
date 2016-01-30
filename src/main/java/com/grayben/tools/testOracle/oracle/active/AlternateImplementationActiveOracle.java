c58a06425218fc0e7c2855dd9ac9b82408581d69
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 12:26:17 +1100

    Comment AlternateImplementationActiveOracle

--- src/main/java/com/grayben/tools/testOracle/oracle/active/AlternateImplementationActiveOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/active/AlternateImplementationActiveOracle.java
@@ -3,7 +3,16 @@ package com.grayben.tools.testOracle.oracle.active;
 import java.util.function.Function;
 
 /**
- * Created by beng on 28/01/2016.
+ * An active oracle that uses an alternative implementation to determine the expected output.
+ * <p>
+ * Ideally, the alternative implementation may be a previous version of the system under test which itself has
+ * been well tested, or a simplified implementation which is only a partial oracle (only determines the output
+ * over a restricted input domain).
+ * <p>
+ * Created by Ben Gray on 28/01/2016.
+ * @see com.grayben.tools.testOracle.SystemUnderTest
+ * @param <I> the input type
+ * @param <O> the output type
  */
 final public class AlternateImplementationActiveOracle<I, O> implements ActiveOracle<I, O> {
 
