06646e0f4c0651570d69bf5d9c5fc77323747962
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 14:31:29 +1100

    Document PairGenerator

--- src/main/java/com/grayben/tools/testOracle/testContainer/PairGenerator.java
+++ src/main/java/com/grayben/tools/testOracle/testContainer/PairGenerator.java
@@ -5,7 +5,14 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.function.Function;
 
 /**
- * Created by beng on 30/01/2016.
+ * A facade for the {@link Function} interface whereby the output type is a pair of generic types.
+ * <p>
+ * This interface is defined to increase the readability of code using the functionality provided.
+ * <p>
+ * Created by Ben Gray on 30/01/2016.
+ * @param <P>
+ * @param <X>
+ * @param <Y>
  */
 public interface PairGenerator<P, X, Y> extends Function<P, Pair<X, Y>> {
 }
