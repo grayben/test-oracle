5ce2a6dd1a45239fff88d964aed5361b08bbb7f2
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 13:51:29 +1100

    Document DiscreteCaseActiveOracle

--- src/main/java/com/grayben/tools/testOracle/oracle/active/DiscreteCaseActiveOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/active/DiscreteCaseActiveOracle.java
@@ -3,12 +3,23 @@ package com.grayben.tools.testOracle.oracle.active;
 import java.util.Map;
 
 /**
- * Created by beng on 28/01/2016.
+ * An active oracle capable of computing the expected output of a finite number of input cases.
+ * <p>
+ * Created by Ben Gray on 28/01/2016.
+ * @param <I> the input type
+ * @param <O> the output type
  */
 final public class DiscreteCaseActiveOracle<I, O> implements ActiveOracle<I, O> {
 
+    /**
+     * Maps a finite set of input cases to their expected outputs
+     */
     private final Map<I, O> casePairs;
 
+    /**
+     * Constructs a {@link DiscreteCaseActiveOracle} relying in the specified map.
+     * @param casePairs
+     */
     public DiscreteCaseActiveOracle(Map<I, O> casePairs) {
         this.casePairs = casePairs;
     }
@@ -17,7 +28,7 @@ final public class DiscreteCaseActiveOracle<I, O> implements ActiveOracle<I, O> 
     public O apply(I input) {
         if (casePairs.containsKey(input) == false){
             throw new IllegalArgumentException(
-                    "The input case given is not covered by this verification provider"
+                    "The input case given is not covered by this oracle"
             );
         }
         return casePairs.get(input);
