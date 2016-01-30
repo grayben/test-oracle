cbf017a73332889ad492ee4940e4e712f7499f3d
Ben Gray <gray.ben.m@gmail.com>
2016-01-30 14:26:20 +1100

    Document Oracles

--- src/main/java/com/grayben/tools/testOracle/oracle/Oracles.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/Oracles.java
@@ -4,6 +4,11 @@ import com.grayben.tools.testOracle.oracle.active.ActiveOracle;
 import com.grayben.tools.testOracle.oracle.passive.PassiveOracle;
 
 /**
+ * A utility class containing static methods with which to manipulate test oracles defined in
+ * {@link com.grayben.tools.testOracle.oracle}.
+ * <p>
+ * This class may not be instantiated.
+ * <p>
  * Created by Ben Gray on 30/01/2016.
  */
 public class Oracles {
@@ -11,11 +16,11 @@ public class Oracles {
     private Oracles(){}
 
     /**
-     *
-     * @param activeOracle
-     * @param <I>
-     * @param <O>
-     * @return
+     * Converts the specified {@link ActiveOracle} into an equivalent {@link PassiveOracle}.
+     * @param activeOracle the oracle to convert
+     * @param <I> the input type
+     * @param <O> the output type
+     * @return a {@link PassiveOracle} equivalent to the specified {@link ActiveOracle}
      */
     public static <I, O> PassiveOracle<I, O> passiveOracle(ActiveOracle<I, O> activeOracle){
         return (input, actualOutput) -> actualOutput.equals(activeOracle.apply(input));
