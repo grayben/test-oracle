a13da8d7d819e892bf39f48d95e97f87b944f47b
Ben Gray <gray.ben.m@gmail.com>
2016-02-05 10:39:58 +1100

    Add Builder to SystemUnderTest

--- src/main/java/com/grayben/tools/testOracle/SystemUnderTest.java
+++ src/main/java/com/grayben/tools/testOracle/SystemUnderTest.java
@@ -1,9 +1,18 @@
 package com.grayben.tools.testOracle;
 
+import com.grayben.tools.math.function.FunctionBuilder;
+
 import java.util.function.Function;
 
 /**
  * Created by beng on 29/01/2016.
  */
 public interface SystemUnderTest<I, O> extends Function<I, O> {
+
+    class Builder<I, O> extends FunctionBuilder<I, O> {
+
+        public Builder(SystemUnderTest<I, O> systemUnderTest) {
+            super(systemUnderTest);
+        }
+    }
 }
