78d3c021bc8ea0dc2202e3b3f59b035aebb42fbc
Ben Gray <gray.ben.m@gmail.com>
2016-02-05 10:38:53 +1100

    Use BiPredicateBuilder for PassiveOracle.Builder

--- src/main/java/com/grayben/tools/testOracle/oracle/passive/PassiveOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/passive/PassiveOracle.java
@@ -1,11 +1,19 @@
 package com.grayben.tools.testOracle.oracle.passive;
 
+import com.grayben.tools.math.function.builder.BiPredicateBuilder;
+
 import java.util.function.BiPredicate;
 
 /**
  * Created by beng on 28/01/2016.
  */
 public interface PassiveOracle<I, O> extends BiPredicate<I, O> {
+
+    class Builder<I, O> extends BiPredicateBuilder<I, O> {
+        public Builder(PassiveOracle<I, O> function) {
+            super(function);
+        }
+    }
 
     @Override
     boolean test(I input, O actualOutput);
