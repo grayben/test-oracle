f6c22741610dea7db8e2e3da2402751a3fee02f2
Ben Gray <gray.ben.m@gmail.com>
2016-02-08 15:27:02 +1100

    Document ActiveOracle

--- src/main/java/com/grayben/tools/testOracle/oracle/active/ActiveOracle.java
+++ src/main/java/com/grayben/tools/testOracle/oracle/active/ActiveOracle.java
@@ -5,18 +5,33 @@ import com.grayben.tools.math.function.builder.FunctionBuilder;
 import java.util.function.Function;
 
 /**
+ * Given an input, outputs the expected output of a related system under test.
+ * <p>
  * A simple facade for {@link Function} which defines an active test oracle.
  * <p/>
  * Created by Ben Gray on 30/01/2016.
- * @param <I> the input type
- * @param <O> the output type
+ *
+ * @param <I> the type of input to the related system under test
+ * @param <O> the type of output from the related system under test
  */
 public interface ActiveOracle<I, O> extends Function<I, O> {
 
+    /**
+     * A builder. Intended to provide assistance in incrementally constructing
+     * active oracles in future releases.
+     *
+     * @param <I> the type of input to the related system under test
+     * @param <O> the type of output from the related system under test
+     */
     class Builder<I, O> extends FunctionBuilder<I, O> {
 
-        public Builder(ActiveOracle<I, O> function) {
-            super(function);
+        /**
+         * Construct a builder based upon the specified active oracle.
+         *
+         * @param activeOracle the active oracle upon which to base the builder.
+         */
+        public Builder(ActiveOracle<I, O> activeOracle) {
+            super(activeOracle);
         }
     }
 
@@ -24,7 +39,7 @@ public interface ActiveOracle<I, O> extends Function<I, O> {
      * Determine the expected output on a given input.
      * <p>
      * @param input the input
-     * @return the expected output for {@code input}
+     * @return the expected output
      */
     @Override
     O apply(I input);
